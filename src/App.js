43d24fcae6c6bacdf664fe33948afafd4b8c5e88
del <rodelcdavid@gmail.com>
2021-09-07 14:27:36 +0800

    Revert "trial fix for gh pages routing - hashrouter"
    This reverts commit 2b9629a133d82557512256b7ad8bfb6daccfa9bb.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {HashRouter as Router, Switch, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Nav from './components/Nav';
 import Home from './components/Home';
 import Motivation from './components/Motivation';
